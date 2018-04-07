027eea112ffb0c6d25d1b3286ccf2e81c79bb902
George Duan <george.duan@linecorp.com>
2018-04-07 18:45:14 +0800

    fix padding issue

--- src/utils/TimeUtils.js
+++ src/utils/TimeUtils.js
@@ -1,7 +1,8 @@
 
 
 const addPaddingZero = (s) => {
-  return s > 10 ? (s + '') : ('0' + s)
+  if (s == 0) return '00'
+  return s >= 10 ? (s + '') : ('0' + s)
 }
 
 export const toHourMins = (unix) => {
